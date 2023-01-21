7e9869ce696d84f9f87fe72867724042eccde2fe
syhanjin <2819469337@qq.com>
2023-01-21 21:16:51 +0800

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -7,10 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
+# ==============================================================================
+#  Copyright (C) 2023 Sakuyark, Inc. All Rights Reserved                       =
+#                                                                              =
+#    @Time : 2023-1-21 21:16                                                   =
+#    @Author : hanjin                                                          =
+#    @Email : 2819469337@qq.com                                                =
+#    @File : wsgi.py                                                           =
+#    @Program: backend                                                         =
+# ==============================================================================
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.deploy')
 
 application = get_wsgi_application()
