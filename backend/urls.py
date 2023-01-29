ff07277ffd2f1b91c9cfcbaf60b1709d615cc3d0
syhanjin <2819469337@qq.com>
2023-01-29 18:36:46 +0800

    0.1.7

--- backend/urls.py
+++ backend/urls.py
@@ -1,24 +1,25 @@
 # ==============================================================================
 #  Copyright (C) 2023 Sakuyark, Inc. All Rights Reserved                       =
 #                                                                              =
-#    @Time : 2023-1-22 20:11                                                   =
+#    @Time : 2023-1-29 18:0                                                    =
 #    @Author : hanjin                                                          =
 #    @Email : 2819469337@qq.com                                                =
 #    @File : urls.py                                                           =
 #    @Program: backend                                                         =
 # ==============================================================================
+import hutao.views
+import tokenAuth.views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
 from rest_framework_nested import routers
-
-import hutao.views
 
 router = routers.DefaultRouter()
 router.register(r'info', hutao.views.InfoViewSet)
 router.register(r'voices', hutao.views.VoicesViewSet)
 router.register(r'letter', hutao.views.LettersViewSet)
 router.register(r'album', hutao.views.AlbumViewSet)
+router.register(r'token', tokenAuth.views.TokenViewSet)
 album_router = routers.NestedSimpleRouter(router, r"album", lookup="album")
 album_router.register(r"image", hutao.views.AlbumImageViewSet)
 
--- tokenAuth/views.py
+++ tokenAuth/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+# ==============================================================================
+#  Copyright (C) 2023 Sakuyark, Inc. All Rights Reserved                       =
+#                                                                              =
+#    @Time : 2023-1-29 18:21                                                   =
+#    @Author : hanjin                                                          =
+#    @Email : 2819469337@qq.com                                                =
+#    @File : views.py                                                          =
+#    @Program: backend                                                         =
+# ==============================================================================
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from tokenAuth.models import Token
+from tokenAuth.permissions import TokenAuth
+
+
+class TokenViewSet(viewsets.GenericViewSet):
+    queryset = Token.objects.all()
+    permission_classes = [TokenAuth]
+    lookup_field = "token"
+
+    @action(methods=["post"], detail=False)
+    def auth(self, request, *args, **kwargs):
+        return Response(data={"token": request.data["token"]})
